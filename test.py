668fe1edc46e0dec34ffb5c82b17f98cd074e9e9
Max <max.worrall@education.nsw.gov.au>
2023-03-26 19:23:33 +1100

    added first unittest :)

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 import unittest
+import side as s
+
+class TestExample(unittest.TestCase):
+    
+    def test_closest_num(self):
+        self.assertEqual(s.closest_num(list(range(10)), 2), 2) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(s.closest_num(list(range(0, 10, 2)), 7), 7) # [0, 2, 4, 6, 8] 
+        self.assertEqual(s.closest_num(list(range(0, 10, 4)), 3), 4) # [0, 4, 8]
+        self.assertEqual(s.closest_num(list(range(0, 10, 5)), 1), 0) # [0, 5]
+        self.assertEqual(s.closest_num(list(range(0, 10, 5)), 3), None) # [0, 5]
+        self.assertEqual(s.closest_num(list(range(0, 10, 5)), 4), 5) # [0, 5]
+
+if __name__ == '__main__':
+    unittest.main()
