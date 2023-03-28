4adf7f2afbca244b1b8beacb69628cecd5f1e90d
Max Worrall <max.worrall@education.nsw.gov.au>
2023-03-28 15:51:49 +1100

    added debug.py, made it the tkinter layout we've all been dreaming of

--- debug.py
+++ debug.py
@@ -3,6 +3,43 @@ import tkinter as tk
 
 #g = side.Game()
 
+def go(args=None):
+    print('go', args)
+
+def debug(args=None):
+    print('debug', args)
+
+def search():
+    print('search')
+
+def save():
+    print('save')
+
+def show_debug():
+    top = tk.Toplevel(root)
+    l3 = tk.Label(top, text='Debug window', relief='solid')
+    #l3.grid(row=1, column=1, padx=10, pady=10)
+    l3.pack(side='top', fill='x', pady=10, padx=10)
+    
+    btn3 = tk.Button(top, text='search for things to debug', command=search)
+    #btn3.grid(row=2, column=2)
+    btn3.pack(side='top', fill='x', pady=10, padx=10)
+    
+    debuginp = tk.Entry(top)
+    debuginp.pack(fill='both', padx=10, pady=10)
+    
+    debuginp.bind('<Return>', debug)
+    
+    btn4 = tk.Button(top, text='debug', command=debug)
+    #btn4.grid(row=2, column=2)
+    btn4.pack(fill='x', pady=10, padx=10)
+    
+    btn5 = tk.Button(top, text='save', command=save)
+    #btn5.grid(row=2, column=2)
+    btn5.pack(side='bottom', fill='x', pady=10, padx=10)
+
+    top.mainloop()
+
 root = tk.Tk()
 
 #title_text = '"%s": by %s\n%s\nPress enter to start' % (g.fc['card']['title'], g.fc['card']['author'], g.fc['card']['description'])
@@ -20,7 +57,7 @@ inp = tk.Entry(root, relief='sunken')
 #inp.grid(row=2, column=1, padx=10, pady=10)
 inp.pack(side='top', fill='x', pady=10, padx=10)
 
-btn1 = tk.Button(root, text='go')
+btn1 = tk.Button(root, text='go', command=go)
 #btn1.grid(row=2, column=2)
 btn1.pack(fill='x', pady=10, padx=10)
 
@@ -36,4 +73,5 @@ l2 = tk.Label(root, text='Logs:')
 #l2.grid(row=3, column=0)
 l2.pack(side='bottom')
 
+inp.bind('<Return>', go)
 root.mainloop()
