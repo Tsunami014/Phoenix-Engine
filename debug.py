ccf3e539b163c410ec54878adb0abfb2370b794c
Max Worrall <max.worrall@education.nsw.gov.au>
2023-03-29 08:43:29 +1100

    doesn't work yet

--- debug.py
+++ debug.py
@@ -1,10 +1,55 @@
-#import side
+import side as s
 import tkinter as tk
 
-#g = side.Game()
+g = s.Game()
 
+print('setting up tkinter...')
+
+inps = ['']
+i = 0
+
+def up(args=None):
+    print(args)
+    if i > 0:
+        if inp.get() != inps[i]:
+            inps[len(inps)-1] = inp.get()
+            i = len(inps)-1
+        i -= 1
+        inp.delete(0, len(inp.get()))
+        inp.insert(0, inps[i])
+
+def dwn(args=None):
+    print(args)
+    if i < len(inps):
+        if inp.get() != inps[i]:
+            inps[len(inps)-1] = inp.get()
+            i = len(inps)-1
+        i += 1
+        inp.delete(0, len(inp.get()))
+        inp.insert(0, inps[i])
+
+#args in these cases are just the keybinds when you press enter.
 def go(args=None):
-    print('go', args)
+    croom = g.fc['rooms'][str(g.roomnum)]
+    if inp.get() != '':
+        inps[len(inps)-1] = inp.get()
+        inps.append('')
+        i = len(inps)
+        inp.select_clear()
+        logs.config(text="\n".join([str(i) for i in g(inp.get(), g.roomnum, croom['objects'])]))
+    game.config(text=croom['name'].capitalize()+'\n'+croom['description'])
+    
+    #How the second line works is it says you can not exit if there are no exits otherwise it states all the exits and the direction of exit.
+    #How the 3 simmilar statements work: they basically make a string: "[item1, item2, item3]" for each item in the room's items that are of a certain type.
+    txt = "It is%s dark.\n" % ("" if croom['dark'] else "n't") + \
+        "You can " + ('not exit\n' if len(croom['exits']) == 0 else ('exit ' + ", ".join(["%s towards %s" % (s.pos[int(i)], \
+                g.fc["rooms"][str(croom['exits'][i])]["name"]) for i in croom['exits']]) + '\n') + \
+        "There are these objects: " +         '['+"".join([i['identifier']+", " if i['type'] == 5 else '' for i in croom['objects']])+']\n' + \
+        "You can see: " +                     '['+"".join([i['identifier']+", " if i['type'] == 6 else '' for i in croom['objects']])+']\n' + \
+        "There are these people/monsters: " + '['+"".join([i['identifier']+", " if i['type'] == 4 else '' for i in croom['objects']])+']\n')
+    subgame.config(text=txt)
+    inp.delete(0, len(inp.get()))
+    pass
 
 def debug(args=None):
     print('debug', args)
@@ -16,13 +61,12 @@ def save():
     print('save')
 
 def show_debug():
+    global top, l3, btn3, debuginp, btn4, btn5
     top = tk.Toplevel(root)
     l3 = tk.Label(top, text='Debug window', relief='solid')
-    #l3.grid(row=1, column=1, padx=10, pady=10)
     l3.pack(side='top', fill='x', pady=10, padx=10)
     
     btn3 = tk.Button(top, text='search for things to debug', command=search)
-    #btn3.grid(row=2, column=2)
     btn3.pack(side='top', fill='x', pady=10, padx=10)
     
     debuginp = tk.Entry(top)
@@ -31,47 +75,45 @@ def show_debug():
     debuginp.bind('<Return>', debug)
     
     btn4 = tk.Button(top, text='debug', command=debug)
-    #btn4.grid(row=2, column=2)
     btn4.pack(fill='x', pady=10, padx=10)
     
     btn5 = tk.Button(top, text='save', command=save)
-    #btn5.grid(row=2, column=2)
     btn5.pack(side='bottom', fill='x', pady=10, padx=10)
 
     top.mainloop()
 
 root = tk.Tk()
 
-#title_text = '"%s": by %s\n%s\nPress enter to start' % (g.fc['card']['title'], g.fc['card']['author'], g.fc['card']['description'])
-title_text = "Hello! This is where the game text would go,\nbut it isn't here because it makes testing the interface harder."
+title_text = '"%s": by %s\n%s\nPress go to start' % (g.fc['card']['title'], g.fc['card']['author'], g.fc['card']['description'])
 
-game = tk.Label(root, text=title_text, relief='ridge')
-#game.grid(row=1, column=1, padx=10, pady=10)
+game = tk.Label(root, text=title_text, relief='ridge', wraplength=400)
 game.pack(side='top', fill='x', pady=10, padx=10)
 
-l1 = tk.Label(root, text='Input:')
-#l1.grid(row=2, column=0)
+subgame = tk.Label(root, text='', relief='ridge', wraplength=400)
+subgame.pack(side='top', fill='x', pady=10, padx=10)
+
+l1 = tk.Label(root, text='What do you do?')
 l1.pack()
 
 inp = tk.Entry(root, relief='sunken')
-#inp.grid(row=2, column=1, padx=10, pady=10)
 inp.pack(side='top', fill='x', pady=10, padx=10)
 
 btn1 = tk.Button(root, text='go', command=go)
-#btn1.grid(row=2, column=2)
 btn1.pack(fill='x', pady=10, padx=10)
 
 btn2 = tk.Button(root, text='show debug window', command=show_debug)
-#btn2.grid(row=2, column=2)
 btn2.pack(side='bottom', fill='x', pady=10, padx=10)
 
-logs = tk.Label(root, text='Logs go here', relief='groove')
-#logs.grid(row=3, column=2, padx=10, pady=10)
+logs = tk.Label(root, text='Logs go here', relief='groove', wraplength=400)
 logs.pack(side='bottom', padx=10, pady=10, fill='x')
 
 l2 = tk.Label(root, text='Logs:')
-#l2.grid(row=3, column=0)
 l2.pack(side='bottom')
 
 inp.bind('<Return>', go)
+inp.bind('Up', up)
+inp.bind('Down', dwn)
+s.clear()
+go()
+print('complete!')
 root.mainloop()
