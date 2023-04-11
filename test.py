596f4fa34cb73ff1d7e299df0ffc0f4533f5abdc
Max Worrall <max.worrall@education.nsw.gov.au>
2023-04-11 10:38:09 +1000

    finalised a test

--- test.py
+++ test.py
@@ -2,6 +2,34 @@ import unittest
 import side as s
 from json import load, loads
 
+"""
+Action code:
+    (split by ';', so '00Hello;01Goodbye' are 2 different statements both executed seperately)
+    
+    First number:
+        0 = print
+        1 = set variable (if not exists then create variable)
+        2 = delete variable
+    
+    Second number:
+        with the set variables:
+            0 = global
+            1 = class
+        with the print:
+            what colour
+        with the delete variables:
+            0 = the object specified
+    
+    Third string (not for first number=2):
+        what variable name/what to print
+    
+    Delimeter: " ~ "
+    
+    Fourth number (only for first number=1):
+        what value to set it to
+            0 = the closest exit to what was said
+"""
+
 class NewTest(unittest.TestCase):
     g = s.Game()
     
@@ -18,12 +46,12 @@ class NewTest(unittest.TestCase):
         for test in tests.keys():
             self.assertEqual(self.g.parse(tests[test]), loads(test))
     
-    """def test_action(self):
-        self.g.hey({'action': 'throw', 'subjobj': ['pot', {'det': 'the'}]}, {'subjobj': [1, ]})
-        
-        tests = load(open('test cases/parse tests.json'))
-        for test in tests.keys():
-            self.assertEqual(self.g.parse(tests[test]), loads(test))"""
+    def test_action(self):
+        #TODO: finish this test
+        self.assertEqual(self.g.run({'action': ['throw'], 'subjobj': [('pot', {'det': 'the'})]}, [{'subjobj': 1, 'action': 'abcdefg'}, {'subj': 2, 'action': 12345}]), 'abcdefg')
+        self.assertEqual(self.g.run({'action': ['say'], 'subj': [('jeff'), ('joe')]}, [{'subjobj': 1, 'action': 'abcdefg'}, {'subj': 2, 'action': 12345}]), '12345')
+        self.assertEqual(self.g.run({'action': ['talk'], 'subj': [('jeff')]}, [{'subjobj': 1, 'action': 'abcdefg'}, {'subj': 2, 'action': 12345}]), None)
+        self.assertEqual(self.g.run({'action': ['die'], 'in': [('hole')]}, [{'subjobj': 1, 'action': 'abcdefg'}, {'subj': 2, 'action': 12345}]), None)
 
 class MainGameTest(unittest.TestCase):
     
