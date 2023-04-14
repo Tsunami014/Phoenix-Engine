ac826bf476bfb8a04f0ebf4b1f0614317ee9da24
Max Worrall <max.worrall@education.nsw.gov.au>
2023-04-14 15:44:53 +1000

    got rid of old unittest

--- test.py
+++ test.py
@@ -76,31 +76,5 @@ class NewTest(unittest.TestCase):
         #self.g.action({'action': ['die'], 'in': [('hole'), ('hole', ['another', 'a'])]}, {'subjobj': 1, 'action': 'abcdefg'})
         #self.assertIn('')
 
-"""class MainGameTest(unittest.TestCase):
-    
-    def test_closest_num(self):
-        self.assertEqual(s.closest_num(list(range(10)), 2), 2) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(s.closest_num(list(range(0, 10, 2)), 6), 6) # [0, 2, 4, 6, 8] 
-        self.assertEqual(s.closest_num(list(range(0, 10, 4)), 3), 4) # [0, 4, 8]
-        self.assertEqual(s.closest_num(list(range(0, 10, 5)), 1), 0) # [0, 5]
-        self.assertEqual(s.closest_num(list(range(0, 10, 5)), 3), None) # [0, 5]
-        self.assertEqual(s.closest_num(list(range(0, 10, 5)), 4), 5) # [0, 5]
-    
-    def test_run_action(self):
-        g = s.Game()
-        g.run_action("C*S*apple = '{1}'", ['a', 'b', 'c'], set_values_3=False)
-        self.assertEqual(g.apple, 'b')
-
-        g.run_action("C*S*apple = {3}", ['a', 'b', [0, 1, 2, 3], None])
-        self.assertEqual(g.apple, 0)
-        
-        g.run_action("C*S*apple = int('{3}')", ['a', 'b', [5, 10], 'c'])
-        self.assertEqual(g.apple, 5)
-        
-        self.assertRaises(IndexError, g.run_action, "C*S*apple = '{5}'", ['a', 'b', [0, 1, 2, 3], None])
-        
-        #self.assertRaises(NameError, g.run_action, "C*S*apple = '{5}'", ['a', 'b', [0, 1, 2, 3], None])
-"""
-
 if __name__ == '__main__':
     unittest.main()
