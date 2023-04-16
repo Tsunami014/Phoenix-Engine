6091e6991e4cee5c920629e65c79a59b101681fe
Max Worrall <max.worrall@education.nsw.gov.au>
2023-04-16 20:04:25 +1000

    added in base html code from https://python-adv-web-apps.readthedocs.io/en/latest/flask_forms.html just in case you needed the reference

--- app.py
+++ app.py
@@ -1,9 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
 app = Flask(__name__)
+app.secret_key = 'tO$&!|0wkamvVia0?n$NqIRVWOG'
 
-@app.route("/")
-def hello():
-	return render_template('app.html')
+# Bootstrap-Flask requires this line
+bootstrap = Bootstrap5(app)
+# Flask-WTF requires this line
+csrf = CSRFProtect(app)
 
+# with Flask-WTF, each web form is represented by a class
+# "NameForm" can change; "(FlaskForm)" cannot
+# see the route for "/" and "index.html" to see how this is used
+class NameForm(FlaskForm):
+    name = StringField('Which actor is your favorite?', validators=[DataRequired(), Length(10, 40)])
+    submit = SubmitField('Submit')
+
+
+# all Flask routes below
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # you must tell the variable 'form' what you named the class, above
+    # 'form' is the variable name used in this template: index.html
+    form = NameForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+    return render_template('app.html', form=form, message=name.lower())
+
+@app.route('/actor/<id>')
+def actor(id):
+    if id == "Unknown":
+        # redirect the browser to the error template
+        return render_template('404.html'), 404
+    else:
+        # pass all the data for the selected actor to the template
+        return render_template('app.html', id_thing=id)
+
+# 2 routes to handle errors - they have templates too
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+#@app.errorhandler(500)
+#def internal_server_error(e):
+#    return render_template('500.html'), 500
+
+
+# keep this as is
 if __name__ == '__main__':
     app.run(debug=True)
