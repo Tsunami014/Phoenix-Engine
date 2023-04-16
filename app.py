0804d5091fdd98af6559005d08289a529051cfb5
Max Worrall <max.worrall@education.nsw.gov.au>
2023-04-17 08:54:26 +1000

    made a bit more personalised, added in map selector for web framework that does not redirect yet

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, SubmitField
+from wtforms import *
 from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
@@ -17,14 +17,31 @@ csrf = CSRFProtect(app)
 # "NameForm" can change; "(FlaskForm)" cannot
 # see the route for "/" and "index.html" to see how this is used
 class NameForm(FlaskForm):
-    name = StringField('Which actor is your favorite?', validators=[DataRequired(), Length(10, 40)])
+    inp = StringField('Input', validators=[DataRequired(), Length(1)])
+    submit = SubmitField('Submit')
+
+class Inputs(FlaskForm):
+    myChoices = ['Forest out', 'hahahahahaAAAAA', 'making sure this works', 'by adding in more stuff'] #input SPECIFIC MAP NAMES HERE, MUST BE THE EXACT NAME OF THE MAP FILES IN THE 'maps/' FOLDER
+    myField = SelectField(u'Field name', choices = myChoices, validators = [DataRequired()])
     submit = SubmitField('Submit')
 
 
 # all Flask routes below
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/', methods = ['GET', 'POST'])
+def chooser():
+    form = Inputs()
+    if form.validate_on_submit():
+        name = form.myField.data
+    try:
+        message = name.lower()
+    except:
+        message = ""
+    return render_template('selector.html', form=form, message=message)
+
+@app.route('/main/<id>', methods=['GET', 'POST'])
+def index(id):
+    print(id)
     # you must tell the variable 'form' what you named the class, above
     # 'form' is the variable name used in this template: index.html
     form = NameForm()
@@ -36,14 +53,14 @@ def index():
         message = ""
     return render_template('app.html', form=form, message=message)
 
-@app.route('/actor/<id>')
-def actor(id):
+@app.route('/save/<id>')
+def save_slot(id):
     if id == "Unknown":
         # redirect the browser to the error template
         return render_template('404.html'), 404
     else:
         # pass all the data for the selected actor to the template
-        return render_template('app.html', id_thing=id)
+        return render_template('app.html')
 
 # 2 routes to handle errors - they have templates too
 
@@ -51,6 +68,7 @@ def actor(id):
 def page_not_found(e):
     return render_template('404.html'), 404
 
+#use this if u want to.... I think you just copy the 404.html and rename it to 500
 #@app.errorhandler(500)
 #def internal_server_error(e):
 #    return render_template('500.html'), 500
