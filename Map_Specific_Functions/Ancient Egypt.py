46d732f34e17e0ee3d66a9c57266a260ba53b562
Imzafish <129023919+Imzafish@users.noreply.github.com>
2023-05-16 18:26:34 +1000

    Other stuff

--- Map_Specific_Functions/Ancient Egypt.py
+++ Map_Specific_Functions/Ancient Egypt.py
@@ -1,47 +1,9 @@
 import random
 import time
-#Map Selection
-
-#Amour Snapping
-#work in progress (ending up not be used anymore)
-def amour_snapping_mechanism(item):
-    helmet_slot_filled=False
-    amour_slot_filled=False
-    charm_slot_filled=False
-   
-    
-    amour_slot=={}
-if "#amour" in item.tag:
-    amour_slot =item
-    amour_slot_filled=True
-elif"#helmet" in item.tag:
-    helmet_slot =item
-    helmet_slot_filled =True
-elif "#charm" in item.tag:
-     charm_slot =item
-     
-
-
+#voicelines
+#work in progress
 def Voicelines():
   print('Vaules')
-
-def Lives():
-  Livecount=3
-  Death=False
-  if Death==True:
-    choice=random.randint (1,5)
-    if choice==(-1):
-      pass
-
-
-
-
-
-
-
-
-
-
 #Swap lock
 
 #Pushing Boulder
@@ -54,7 +16,12 @@ character_choice=input("Choose a character")
 if character_choice=="hi":
      print("filler")
 
-
+#Quicksand
+#The player needs to press the button and then do a quick reaction test to confirm they made it across.
+if button_pressed==True:
+     print("Get ready")
+     time.sleep(1)
+    
 #Spinx code
 print("Welcome Mortal solve this riddle to continue or DIE")
 riddle_num=random.int(1,5)
@@ -62,12 +29,16 @@ if riddle_num==1:
      ans_riddle_1=input("Tutenkhamun's mother has 4 sons, North, East, West and one other, who is the fourth")
      if ans_riddle_1=="Tutenkhamun":
                 print("Correct Answer, proceed")
+     else :
+          print("Wrong Answer")
+          #Kill the player
 elif riddle_num==2:
     ans_riddle_2=input("What can run but cannot walk")
     if ans_riddle_2.lower() =="river":
         print("Correct Answer, proceed")
     else:
         print("Wrong Answer")
+        #Kill the player
 elif riddle_num==3:
     ans_riddle_3=input("What goes from Z to A")
     if ans_riddle_3==(""):
@@ -76,10 +47,11 @@ elif riddle_num==4:
     print("hi")
 else:
     ans_riddle_5=input( "What goes up, but never comes down?")
-    if ans_riddle_5==(""):
+    if ans_riddle_5==("age"):
          print("Correct answer, proceed")
     else:
          print("Wrong Answer")
+        #Kill the player
 
 #The more you take, the more you leave behind. What am I?
 
